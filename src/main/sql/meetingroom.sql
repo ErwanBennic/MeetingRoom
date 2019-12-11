2ab7ef88fe2423060a622cca1844a6889ff49a10
Senniinn <39193668+Senniinn@users.noreply.github.com>
2019-12-11 17:45:47 +0100

    k

--- src/main/sql/meetingroom.sql
+++ src/main/sql/meetingroom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 10 déc. 2019 à 16:23
+-- Généré le :  mer. 11 déc. 2019 à 16:44
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -25,6 +25,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `participant`
+--
+
+DROP TABLE IF EXISTS `participant`;
+CREATE TABLE IF NOT EXISTS `participant` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `reservation_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `reservation_id` (`reservation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `reservation`
 --
 
@@ -40,7 +55,16 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   PRIMARY KEY (`id`),
   KEY `salle_id` (`salle_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `date_debut`, `date_fin`, `nom`, `description`, `salle_id`, `user_id`) VALUES
+(1, '2019-12-03 00:00:00', '2019-12-19 00:00:00', 'resa 1', 'desc', 1, 1),
+(2, '2019-12-11 01:00:00', '2019-12-11 04:00:00', 'resa 2', 'desc 2', 2, 2),
+(3, '2019-12-11 05:00:00', '2019-12-11 09:00:00', 'resa3', 'desc 3', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -53,7 +77,15 @@ CREATE TABLE IF NOT EXISTS `salle` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `lib` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `salle`
+--
+
+INSERT INTO `salle` (`id`, `lib`) VALUES
+(1, '201'),
+(2, '103');
 
 -- --------------------------------------------------------
 
@@ -69,11 +101,25 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(255) NOT NULL,
   `tel` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `fullname`, `email`, `password`, `tel`) VALUES
+(1, 'Jean denis', 'jd@jd.fr', '123', '4355'),
+(2, 'Roger', 'dzd@fe.fr', '123', '2345');
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `participant`
+--
+ALTER TABLE `participant`
+  ADD CONSTRAINT `participant_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`);
 
 --
 -- Contraintes pour la table `reservation`
