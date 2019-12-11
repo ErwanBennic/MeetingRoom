cf54eaa2d0c3f0742745eb43bb8cb5774a015afb
Senniinn <39193668+Senniinn@users.noreply.github.com>
2019-12-11 17:38:56 +0100

    hhhh

--- src/main/java/meetingroom/spring/controleur/ReservationController.java
+++ src/main/java/meetingroom/spring/controleur/ReservationController.java
@@ -43,4 +43,7 @@ public class ReservationController {
 
         return "scan";
     }
+
+
+
 }
--- src/main/java/meetingroom/spring/dao/RerservationDao.java
+++ src/main/java/meetingroom/spring/dao/RerservationDao.java
@@ -1,10 +1,13 @@
 package meetingroom.spring.dao;
 
 import meetingroom.spring.modele.Reservation;
+import meetingroom.spring.modele.Salle;
+import meetingroom.spring.modele.User;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class RerservationDao {
@@ -13,5 +16,29 @@ public class RerservationDao {
 
     public void insertResult(Reservation reservation){
         em.persist(reservation);
+    }
+
+    public User findUser(String email) {
+        List<User> users;
+        users = em.createQuery("select u from User u where u.email = :email", User.class)
+                .setParameter("email", email).getResultList();
+        if (users.size() < 1){
+            return null;
+        } else {
+            User user = users.get(0);
+            return user;
+        }
+    }
+
+    public Salle findSalle(String lib) {
+        List<Salle> salles;
+        salles = em.createQuery("select s from Salle s where s.lib = :lib", Salle.class)
+                .setParameter("lib", lib).getResultList();
+        if (salles.size() < 1){
+            return null;
+        } else {
+            Salle salle = salles.get(0);
+            return salle;
+        }
     }
 }
